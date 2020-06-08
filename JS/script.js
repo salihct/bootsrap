1af80306051fa8c89b934259a5284632957e6ce5
Sanoof Ct <ctsanoo0@gmail.com>
2020-06-08 09:16:00 +0530

    npm part 2

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+$("#carouselButton").click(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+        $("#mycarousel").carousel('pause');
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("span").addClass('fa-play');
+    }
+    else if ($("#carouselButton").children("span").hasClass('fa-play')){
+        $("#mycarousel").carousel('cycle');
+        $("#carouselButton").children("span").removeClass('fa-play');
+        $("#carouselButton").children("span").addClass('fa-pause');                    
+    }
+});
+$("#loginbutton").click(function(){
+    $('#loginModal').modal('toggle')
+});
+$("#reservebutton").click(function(){
+    $('#reserveModal').modal('toggle')
+});
